dffca70f687b57142385d1270caab62f4af0d18e
Covidnuts <135696682+Covidnuts@users.noreply.github.com>
2024-01-03 23:23:35 +0100

    changes

---
+++ index.js
@@ -0,0 +1,8 @@
+let selectedColours = ["red", "orange"];
+console.log(selectedColours);
+
+function greet (name, lastName) {
+console.log("hello world " + name + lastName);
+}
+
+greet(23, "chessy");
